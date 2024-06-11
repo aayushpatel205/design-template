5193359a7da46c9028db1211ce1a0d5b1ddd83ac
aayushpatel205 <aayush2052005@gmail.com>
2024-06-11 18:18:01 +0530

    v1

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,13 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://newsapi.org/v2", // your API base URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
